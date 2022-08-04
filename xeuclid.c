a3e7821fbf713056a1a2226f690c913bb01a479b
Eungyu <ekpark97.dev@gmail.com>
2022-08-05 00:21:57 +0900

    project: backup

--- xeuclid.c
+++ xeuclid.c
@@ -9,42 +9,50 @@ void printBN(const char* msg, BIGNUM* a){
 
 BIGNUM *XEuclid(BIGNUM *x, BIGNUM *y, const BIGNUM *a, const BIGNUM *b)
 {
-        BIGNUM *gcd = BN_new();
+        // ax + by = gcd
+        BIGNUM *a_cur = BN_dup(a);
+        BIGNUM *b_cur = BN_dup(b);
 
-        BIGNUM *a_ = BN_dup(a);
-        BIGNUM *b_ = BN_dup(b);
-
-        BIGNUM *x_0 = BN_new();  BN_dec2bn(&x_0, "1");
-        BIGNUM *x_1 = BN_new();  BN_dec2bn(&x_1, "0");
+        // a * (1) + b * (0) = a -> a * (x_0) + b * (y_0) = a
+        BIGNUM *x_0 = BN_new();  BN_dec2bn(&x_0, "1"); 
         BIGNUM *y_0 = BN_new();  BN_dec2bn(&y_0, "0");
+
+        // a * (0) + b * (1) = b -> a * (x_1) + b * (y_1) = b
+        BIGNUM *x_1 = BN_new();  BN_dec2bn(&x_1, "0");
         BIGNUM *y_1 = BN_new();  BN_dec2bn(&y_1, "1");
 
         BIGNUM *rem = BN_new();
         BIGNUM *dv = BN_new();
+
+        // buf for mul, div.
         BN_CTX *ctx = BN_CTX_new();
 
+        // tmp for mul.
         BIGNUM *mul_dv_p2 = BN_new();
         BIGNUM *mul_dv_q2 = BN_new();
 
-        while (BN_is_zero(b_) != 1){
-                BN_div(dv, rem, a_, b_, ctx);
-                BN_copy(a_, b_);
-                BN_copy(b_, rem);
+        while (!BN_is_zero(b_cur)){
+                // a_cur = b_cur * dv + rem
+                BN_div(dv, rem, a_cur, b_cur, ctx);
 
+                // x = x_0 - (dv * x_1)
                 BN_mul(mul_dv_p2, dv, x_1, ctx);
                 BN_sub(x, x_0, mul_dv_p2);
-                BN_copy(x_0, x_1);
-                BN_copy(x_1, x);
 
+                // y = y_0 - (dv * y_1)
                 BN_mul(mul_dv_q2, dv, y_1, ctx);
                 BN_sub(y, y_0, mul_dv_q2);
+
+                BN_copy(a_cur, b_cur);
+                BN_copy(b_cur, rem);
+                BN_copy(x_0, x_1);
+                BN_copy(x_1, x);
                 BN_copy(y_0, y_1);
                 BN_copy(y_1, y);
         }
         BN_copy(x, x_0);
         BN_copy(y, y_0);
-        BN_copy(gcd, a_);
-        return gcd;
+        return BN_dup(a_cur);
 }
 
 int main (int argc, char *argv[])
